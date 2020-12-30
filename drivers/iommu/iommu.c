bc35f706f78495a8228fd8e085d44ce5aea59e05
Jason Zeng <jason.zeng@intel.com>
2020-12-30 11:56:25 +0800

    iommu: check deferred attach when iommu probe device
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -242,6 +242,15 @@ err_free:
 	return ret;
 }
 
+static bool iommu_is_attach_deferred(struct iommu_domain *domain,
+				     struct device *dev)
+{
+	if (domain->ops->is_attach_deferred)
+		return domain->ops->is_attach_deferred(domain, dev);
+
+	return false;
+}
+
 int iommu_probe_device(struct device *dev)
 {
 	const struct iommu_ops *ops = dev->bus->iommu_ops;
@@ -264,7 +273,8 @@ int iommu_probe_device(struct device *dev)
 	 */
 	iommu_alloc_default_domain(group, dev);
 
-	if (group->default_domain) {
+	if (group->default_domain &&
+	    !iommu_is_attach_deferred(group->default_domain, dev)) {
 		ret = __iommu_attach_device(group->default_domain, dev);
 		if (ret) {
 			iommu_group_put(group);
@@ -770,15 +780,6 @@ out:
 	iommu_put_resv_regions(dev, &mappings);
 
 	return ret;
-}
-
-static bool iommu_is_attach_deferred(struct iommu_domain *domain,
-				     struct device *dev)
-{
-	if (domain->ops->is_attach_deferred)
-		return domain->ops->is_attach_deferred(domain, dev);
-
-	return false;
 }
 
 /**
